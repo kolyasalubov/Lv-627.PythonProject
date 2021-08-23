fd182181f0feea776d6a47bd3cbf869ce4d6a00e
Sofiia Kovalchuk <36841164+CheshirLvova@users.noreply.github.com>
2021-08-23 12:55:31 +0300

    Delete validation.py

--- validation.py
+++
@@ -1,7 +0,0 @@
-def input_natural_number() -> int:
-    number = input("Enter a natural number n: ")
-    if str(number).isdigit() and float(number) == int(number) and int(number) > 0:
-        return int(number)
-    else:
-        print("Wrong input. 0 < natural number <= +∞. Please, try again")
-        input_natural_number()
