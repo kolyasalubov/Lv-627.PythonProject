4275621007fdd6247b36941518738a10ff07c7f5
v-lavrushko <landadang@gmail.com>
2021-08-23 12:25:49 +0300

    initial commit. base class for all tasks and console controller

---
+++ task.py
@@ -0,0 +1,16 @@
+class Task:
+    def __init__(self):
+        self.arg = 1
+        self.description = "This is a base class for all tasks. You shouldn't see this."
+
+    def change_arguments(self, arg):
+        self.arg = arg
+
+    def validate(self):
+        if isinstance(self.arg, int) and self.arg >= 0:
+            return True
+        else:
+            return False
+
+    def solve(self):
+        pass
