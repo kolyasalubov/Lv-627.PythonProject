6e773e986b1d06a1d060dfcb644e1bbb8cb05542
Sofiia Kovalchuk <36841164+CheshirLvova@users.noreply.github.com>
2021-08-23 10:56:31 +0300

    Added solution of tasks
    Bug fixes

--- task088a.py
+++ task088a.py
@@ -1,7 +1,10 @@
+from validation import *
 # 88. Given a natural number n.
 #     а) Find is 3 a digit of number n^2.
+
+
 def find_three_in_record_of_m(self, task: str = "088a") -> str:
-    n = int(input("Enter a natural number n: "))
+    n = int(input_natural_number())
     m = n * n
     if str(m).find('3') > -1:
         return f"3 is a digit of number {n}^2 = {m}"
