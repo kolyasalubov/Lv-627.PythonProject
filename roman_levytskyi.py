faddf8edc8df4e3f7d03cebee3fade8bc5e1632f
l3va <ggwpleva@gmail.com>
2021-08-23 14:35:33 +0300

    added solution class for task 554

---
+++ roman_levytskyi.py
@@ -0,0 +1,39 @@
+from math import sqrt
+from task import Task
+
+
+class Task554(Task):
+    def __init__(self):
+        super()
+        self.description = "Find all pythagorean triples that are lower than input natural number n"
+
+    def solve(self):
+        n = self.arg
+        pythagorean_triples = []
+        for c in range(n):
+            for b in range(c):
+                for a in range(b):
+                    if a ** 2 + b ** 2 == c ** 2:
+                        pythagorean_triples.append((a, b, c))
+        return f"List of all pythagorean_triples below natural number {n} are:\n{pythagorean_triples}"
+
+# class Task187b(Task):
+#     def __init__(self):
+#         super()
+#         self.description = "Find all numbers that are divided by 3 and 5 in input list of natural numbers"
+#
+#     def solve(self):
+#         n = self.arg
+#         counter = len([i for i in n if i % 3 == 0 and i % 5 == 0 and i >= 1])
+#         return f"There is {counter} numbers that are divided by 3 and 5 in list of natural numbers:\n{n}"
+#
+#
+# class Task187c(Task):
+#     def __init__(self):
+#         super()
+#         self.description = "Count how many numbers are squares of even number in input list of natural numbers"
+#
+#     def solve(self):
+#         n = self.arg
+#          counter = len([i for i in numbers if math.sqrt(i) % 2 == 0])
+#          return counter
