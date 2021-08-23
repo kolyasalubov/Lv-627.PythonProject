983a9bad1cab7915b6bdc7b8df308bdffb5af4a4
Sofiia Kovalchuk <36841164+CheshirLvova@users.noreply.github.com>
2021-08-23 10:57:11 +0300

    Added solution of tasks
    Bug fixes

--- task088b.py
+++ task088b.py
@@ -1,7 +1,11 @@
+from validation import *
 # 88. Given a natural number n.
 #     б) Change the order of digits of n by reversing its digits.
+
+
 def reverse_n(self, task: str = "088b") -> str:
-    n = str(input("Enter a natural number n: "))
+    n = input_natural_number()
+    n = str(n)
     # if n[0] == '-':
     #     return int(n[:0:-1]))
     return f"Reversed number = {int(n[::-1])} "
