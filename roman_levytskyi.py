fff2d5692279902194e9c4bc798765858906e0f2
l3va <ggwpleva@gmail.com>
2021-08-24 16:18:21 +0300

    added solution classes for tasks 187b and 187c

--- roman_levytskyi.py
+++ roman_levytskyi.py
@@ -3,9 +3,9 @@ from task import Task
 
 
 class Task554(Task):
-    def __init__(self):
+    def init(self):
         super()
-        self.description = "Find all pythagorean triples that are lower than input natural number n"
+        self.description = "Find all pythagorean triples that lower than input natural number n"
 
     def solve(self):
         n = self.arg
@@ -17,23 +17,30 @@ class Task554(Task):
                         pythagorean_triples.append((a, b, c))
         return f"List of all pythagorean_triples below natural number {n} are:\n{pythagorean_triples}"
 
-# class Task187b(Task):
-#     def __init__(self):
-#         super()
-#         self.description = "Find all numbers that are divided by 3 and 5 in input list of natural numbers"
-#
-#     def solve(self):
-#         n = self.arg
-#         counter = len([i for i in n if i % 3 == 0 and i % 5 == 0 and i >= 1])
-#         return f"There is {counter} numbers that are divided by 3 and 5 in list of natural numbers:\n{n}"
-#
-#
-# class Task187c(Task):
-#     def __init__(self):
-#         super()
-#         self.description = "Count how many numbers are squares of even number in input list of natural numbers"
-#
-#     def solve(self):
-#         n = self.arg
-#          counter = len([i for i in numbers if math.sqrt(i) % 2 == 0])
-#          return counter
+
+class Task187b(Task):
+    def init(self):
+        super()
+        self.description = "Find all numbers that are divided by 3 and 5 in input list of natural numbers"
+
+    def change_arguments(self, *args):
+        self.args = args
+
+    def solve(self):
+        n = self.args
+        counter = len([i for i in n if i % 3 == 0 and i % 5 == 0 and i >= 1])
+        return f"There is {counter} numbers that are divided by 3 and 5 in list of natural numbers:\n{n}"
+
+
+class Task187c(Task):
+    def init(self):
+        super()
+        self.description = "Count how many numbers are squares of even number in input list of natural numbers"
+
+    def change_arguments(self, *args):
+        self.args = args
+
+    def solve(self):
+        n = self.args
+        counter = len([i for i in n if sqrt(i) % 2 == 0])
+        return counter
