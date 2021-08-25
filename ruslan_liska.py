759bd30b38b864b792f08b0fafc8b203843af251
Ruslan Liska <ruslan.liska@gmail.com>
2021-08-25 12:50:36 +0300

    update 178 and 555 tasks

--- ruslan_liska.py
+++ ruslan_liska.py
@@ -4,11 +4,12 @@ from math import factorial
 class Task555(Task):
     def __init__(self):
         super().__init__()
-        self.description = "Given digit is n, get n rows of Pascal triangle"
+        self.description = "Given digit is n, get n rows of Pascal's triangle"
     
     
-    def solve(self, num_of_rows):
-        self.num_of_rows = num_of_rows
+    def solve(self):
+
+        num_of_rows = self.arg
         results = []
         for _ in range(num_of_rows): 
             row = [1]
@@ -17,8 +18,10 @@ class Task555(Task):
                 row.extend([sum(i) for i in zip(last_row, last_row[1:])])
                 row.append(1)
             results.append(row) 
-        for i in results:
-            print(i)
+        # for i in results:
+        value = [i for i in results]
+    
+        return f"Here is your Pascal's Triangle: {value}"
 
 class Task178d(Task):
     def __init__(self):
@@ -30,14 +33,14 @@ class Task178d(Task):
     def change_arguments(self, *args):
         self.args = args
 
-    def solve(self, n):
-        self.n = n
-        return len([1 for i in range(1, len(n) - 1) if n[i] < (n[i - 1] + n[i + 1]) / 2])  
-
+    def solve(self):
+        n = self.args
+        result = len([1 for i in range(1, len(n) - 1) if n[i] < (n[i - 1] + n[i + 1]) / 2])  
+        return f"There is {result} digits which satisfy this condition"
 
 class Task178e(Task):
     def __init__(self):
-        super()
+        super().__init__()
         self.description = """Given list n: a1, ..., an. Get a quantity 
                            of numbers ak of the sequence a1, ..., an, 
                            which satisfy the condition 2**k < ak-1 < k!"""
@@ -45,7 +48,7 @@ class Task178e(Task):
     def change_arguments(self, *args):
         self.args = args
 
-    def solve(self, n):
-        self.n = n
-        return len([1 for i in range(1, len(n) - 1) if 2 ** int(n[i]) < n[i - 1] < factorial([i])])
-
+    def solve(self):
+        n = self.args
+        result = len([1 for i in range(1, len(n) - 1) if 2 ** int(n[i]) < n[i - 1] < factorial([i])])
+        return f"There is {result} numbers which satisfy the condition"
