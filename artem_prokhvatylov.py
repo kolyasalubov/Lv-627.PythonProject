fc032ffc7d764d4f0ed8c34740766e8206744f29
ProkhvatylovArtem <89210528+ProkhvatylovArtem@users.noreply.github.com>
2021-08-28 16:21:12 +0300

    Bug fixes in task 182

--- artem_prokhvatylov.py
+++ artem_prokhvatylov.py
@@ -11,13 +11,15 @@ class Task182(Task):
         number = self.arg
         sum_of_numbers = 0
         count_of_numbers = 0
-        if number is not None:
+        if number is not None and number > 1:
             for i in range(1, number+1):
                 if i % 5 == 0 and i % 7 != 0:
                     sum_of_numbers += i
                     count_of_numbers += 1
             return ("In sequence 1...{0} is {1} members that are divisible by 5 "
                 "and not divisible by 7 and their sum is {2}").format(number, count_of_numbers, sum_of_numbers)
+        elif number == 1:
+            print("Your sequence contains one element.")
 
 
 class Task560(Task):
