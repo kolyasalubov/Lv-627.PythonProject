0b161b99382fd843131cc9270b35eeb78b72785a
Oleh Hnyp <lefk6230@gmail.com>
2021-08-30 20:36:10 +0300

    Added tests to tasks 182, 323, 560.

---
+++ Tasks/test_task_323.py
@@ -0,0 +1,11 @@
+from Tasks.task_323 import task_560
+
+
+def test_cases_task323_solve():
+    cases = [
+        {'number': 16, 'result': [1, 3, 5, 7, 9, 11, 13, 15]},
+        {'number': 5, 'result': [1, 2, 3, 4]},
+        {'number': 20, 'result': [1, 3, 7, 9, 11, 13, 17, 19]},
+    ]
+    for case in cases:
+        assert task_560.run_body(case.get('number')) == case.get('result')
