bf81afa5258e7c6af992e8f8b05ee36f11040830
MykhailoGrynenko <banehallovvv1@gmail.com>
2021-08-30 16:15:04 +0300

    A mistake has been fixed.

--- Tasks/test_task_330b.py
+++ Tasks/test_task_330b.py
@@ -2,12 +2,12 @@ from Tasks.task_330 import task_330
 
 
 def test_task_330():
-    assert test_task_330.run_body(5) == []
-    assert test_task_330.run_body(6) == []
-    assert test_task_330.run_body(7) == [6]
-    assert test_task_330.run_body(28) == [6]
-    assert test_task_330.run_body(29) == [6, 28]
-    assert test_task_330.run_body(496) == [6, 28]
-    assert test_task_330.run_body(497) == [6, 28, 496]
-    assert test_task_330.run_body(10000) == [6, 28, 496, 8128]
-    assert test_task_330.run_body(100000) == [6, 28, 496, 8128]
+    assert task_330.run_body(5) == []
+    assert task_330.run_body(6) == []
+    assert task_330.run_body(7) == [6]
+    assert task_330.run_body(28) == [6]
+    assert task_330.run_body(29) == [6, 28]
+    assert task_330.run_body(496) == [6, 28]
+    assert task_330.run_body(497) == [6, 28, 496]
+    assert task_330.run_body(10000) == [6, 28, 496, 8128]
+    assert task_330.run_body(100000) == [6, 28, 496, 8128]
