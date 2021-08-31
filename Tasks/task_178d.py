13539097964ddfe8bad99e72e3112275fdaf6ce0
Ruslan Liska <ruslan.liska@gmail.com>
2021-08-31 12:05:55 +0300

    changed description for the task

--- Tasks/task_178d.py
+++ Tasks/task_178d.py
@@ -2,8 +2,8 @@ from task import Task
 
 def _task(*args) -> str:
     """Given list n: a1, ..., an. Get a quantity
-                               of numbers ak of the sequence a1, ..., an,
-                               which satisfy the condition 2**k < ak-1 < k!"""
+                           of numbers ak of the sequence a1, ..., an,
+                           which satisfy the condition ak < (ak-1 + ak+1)/2"""
     n = args
     result = len([1 for i in range(1, len(n) - 1) if n[i] < (n[i - 1] + n[i + 1]) / 2])
     return f"There is {result} digits in sequence {n} which satisfy this condition"
@@ -12,8 +12,7 @@ task_178d = Task(
     name="task_178d",
     description="""Given list n: a1, ..., an. Get a quantity 
                            of numbers ak of the sequence a1, ..., an, 
-                           which satisfy the condition 2**k < ak-1 < k!
-                """,
+                           which satisfy the condition ak < (ak-1 + ak+1)/2"""
     body=_task,
     test="Tasks/test_task_182.py"
 )
