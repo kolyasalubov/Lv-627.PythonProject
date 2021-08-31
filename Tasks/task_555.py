f7d4305966413ac7792f1c831c8579943e29ac26
Ruslan Liska <ruslan.liska@gmail.com>
2021-08-31 10:32:12 +0300

    added task 555

---
+++ Tasks/task_555.py
@@ -0,0 +1,20 @@
+from task import Task
+
+def _task (number: int) -> list:
+    results = []
+    for _ in range(number):
+        row = [1]
+        if results:
+            last_row = results[-1]
+            row.extend([sum(i) for i in zip(last_row, last_row[1:])])
+            row.append(1)
+        results.append(row)
+    value = [i for i in results]
+    return value
+
+
+task_555 = Task(
+    name="task_555",
+    body=_task,
+    test="Tasks/test_task_555.py"
+)
