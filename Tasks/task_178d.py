fc9828355dba6f819dbb62e66d728ae35beae303
Ruslan Liska <ruslan.liska@gmail.com>
2021-09-01 09:41:06 +0300

    Modified 178d task

--- Tasks/task_178d.py
+++ Tasks/task_178d.py
@@ -5,9 +5,13 @@ def _task(*args) -> str:
                            of numbers ak of the sequence a1, ..., an,
                            which satisfy the condition ak < (ak-1 + ak+1)/2"""
     n = args
-    result = len([1 for i in range(1, len(n) - 1) if n[i] < (n[i - 1] + n[i + 1]) / 2])
-    return f"There is {result} digits in sequence {n} which satisfy the condition ak < (ak-1 + ak+1)/2"
-
+    if len(n) == 1:
+        return "You have given only 1 argument"
+    elif len(n) > 1:
+        result = len([1 for i in range(1, len(n) - 1) if n[i] < (n[i - 1] + n[i + 1]) / 2])
+        return f"There is {result} digits in sequence {n} which satisfy the condition ak < (ak-1 + ak+1)/2"
+    else:
+        return "No arguments were given"
 task_178d = Task(
     name="task_178d",
     description="""Given list n: a1, ..., an. Get a quantity 
@@ -16,3 +20,4 @@ task_178d = Task(
     body=_task,
     test="Tasks/test_task_182.py"
 )
+
