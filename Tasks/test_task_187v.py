8ee5e9a873de0d199afa27b1836a806b7375cbe2
Ruslan Liska <ruslan.liska@gmail.com>
2021-09-13 18:18:06 +0300

    updated test for 187b task with raising errors and added test gor 187v task

--- Tasks/test_task_187v.py
+++ Tasks/test_task_187v.py
@@ -7,17 +7,15 @@ import pytest
 class TestTask187b(TestCase):
 
     def test_task_187b(self):
-        test1 = task_187b.run_body([1,2,4,5,6,78)
-        self.assertEqual(test1, 0)
+        test1 = task_187b.run_body([1,4,15,45,300,58,75])
+        self.assertEqual(test1, 1)
 
-        test2 = task_187b.run_body([1,2,15,45])
+        test2 = task_187b.run_body([16,4,5,25,78])
         self.assertEqual(test2, 2)
 
-        test3 = task_187b.run_body([1,2,15,45,300,58,75])
-        self.assertEqual(test3, 4)
+        test3 = task_187b.run_body([16,4,5,25,78,36])
+        self.assertEqual(test3, 3)
 
     def test_errors_task_187b(self):
         with pytest.raises(TaskError):
             task_187b.run_body([])
-
-
