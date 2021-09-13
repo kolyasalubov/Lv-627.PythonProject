4cfa37a7bed95e68642ab83ab8e600245d80191f
Ruslan Liska <ruslan.liska@gmail.com>
2021-09-13 18:12:06 +0300

    added tests for task 187b

--- Tasks/test_task_187b.py
+++ Tasks/test_task_187b.py
@@ -7,21 +7,19 @@ import pytest
 class TestTask187b(TestCase):
 
     def test_task_187b(self):
-        test1 = task_187b.run_body(1)
+        test1 = task_187b.run_body([1,2,4,5,6,78)
         self.assertEqual(test1, 0)
 
-        test2 = task_187b.run_body(10)
-        self.assertEqual(test2, 0)
+        test2 = task_187b.run_body([1,2,15,45])
+        self.assertEqual(test2, 2)
 
-        test3 = task_187b.run_body(1000)
-        self.assertEqual(test3, 0)
+        test3 = task_187b.run_body([1,2,15,45,300,58,75])
+        self.assertEqual(test3, 4)
 
     def test_errors_task_187b(self):
         with pytest.raises(TaskError):
-            task_187b.run_body(-1)
+            task_187b.run_body([])
 
         with pytest.raises(TypeError):
             task_187b.run_body("1")
 
-        with pytest.raises(TaskError):
-            task_187b.run_body(0)
