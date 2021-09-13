2fdc58017e18d3be6176301a9264b282deec5a3c
Ruslan Liska <ruslan.liska@gmail.com>
2021-09-13 13:39:21 +0300

    Updated task178d with check of the natural number and updated test_name

--- Tasks/task_178d.py
+++ Tasks/task_178d.py
@@ -1,15 +1,22 @@
 from task import Task
+from task_error import TaskError
 
 
 def _task(number: int) -> str:
     """Given list n: a1, ..., an. Get a quantity
                            of numbers ak of the sequence a1, ..., an,
                            which satisfy the condition ak < (ak-1 + ak+1)/2"""
+
     n = []
-    for i in range(1, number + 1):
-        n.append(i)
-    result = len([1 for i in range(1, len(n) - 1) if n[i] < (n[i - 1] + n[i + 1]) / 2])
-    return f"There is {result} digits in given sequence which satisfy the condition ak < (ak-1 + ak+1)/2"
+    if number is not None and number > 1:
+        for i in range(1, number + 1):
+            n.append(i)
+        result = len([1 for i in range(1, len(n) - 1) if n[i] < (n[i - 1] + n[i + 1]) / 2])
+        return f"There is {result} digits in given sequence which satisfy the condition ak < (ak-1 + ak+1)/2"
+    elif number == 1:
+        return "You sequence contains only 1 element"
+    else:
+        raise TaskError
 
 
 task_178d = Task(
@@ -18,8 +25,5 @@ task_178d = Task(
                            of numbers ak of the sequence a1, ..., an, 
                            which satisfy the condition ak < (ak-1 + ak+1)/2""",
     body=_task,
-    test="Tasks/test_task_182.py"
+    test="Tasks/test_task_178d.py"
 )
-
-
-print(_task(10000))
