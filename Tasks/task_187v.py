701c730e3e4c71a197f9d43b39980e7dccaaf282
l3va <ggwpleva@gmail.com>
2021-09-15 04:00:21 +0300

    fix input of not natural number and reworked tasks 187b and 187v

--- Tasks/task_187v.py
+++ Tasks/task_187v.py
@@ -1,12 +1,15 @@
 from task import Task
-from typing import List
 from math import sqrt
+from task_error import TaskError
 
 
-def _task(numbers: List[int]) -> int:
+def _task(number: int) -> int:
     """
-    Calculate quantity of numbers that are squares of even numbers in given list of natural number.
+    Calculate quantity of numbers that are squares of even numbers in list given by number n(a1, ..., an).
     """
+    if number < 1:
+        raise TaskError
+    numbers = [i for i in range(1, number + 1)]
     counter = len([i for i in numbers if sqrt(i) % 2 == 0 and i >= 1])
     return counter
 
@@ -15,7 +18,7 @@ task_187v = Task(
     name="task_187v",
     description=
     """
-    Calculate quantity of numbers that are squares of even numbers in given list of natural number.
+    Calculate quantity of numbers that are squares of even numbers in list given by number n(a1, ..., an).
     """,
     body=_task,
     test="Tasks/test_task_187v.py"
